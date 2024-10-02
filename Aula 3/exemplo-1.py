4183f180e8ed1e7e5f1efb61708a7eb12a02fbae
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-02 19:50:24 -0300

    aula 3 - exemplo 1

---
+++ Aula 3/exemplo-1.py
@@ -0,0 +1,6 @@
+print("Vamos calcular o imposto!")
+
+valor_produto = float(input("Valor do produto: R$"))
+imposto = valor_produto * 0.1
+
+print(f"O valor que você terá que pagar de imposto desse produto é: {imposto}")
