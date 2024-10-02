3c75525c6c7f67e6ea36a14b4989fc640ccacb13
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-02 19:24:56 -0300

    exercício - aula 2
    print()

---
+++ Aula 2/exemplo1.py
@@ -0,0 +1,10 @@
+# Esses serão os passos do pirata
+print("Andar para a direita.")
+print("Andar para a esquerda.")
+
+# Isso é um comentário
+
+print("Andar para baixo.")
+
+print("Este é o caminho para o mapa do tesouro.")
+print("Procurar o X e cavar..")
