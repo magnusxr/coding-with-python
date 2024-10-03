c6a6bd40e0a215d2a89d8c0d1510a4de3c2545b8
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-02 21:43:42 -0300

    exercício 3 - tamanho de sapato e fucinheira

---
+++ Aula 3/exercício-3.py
@@ -0,0 +1,6 @@
+coleira = int(input("Qual o tamanho da coleira do cachorro? "))
+
+sapato = 2 * coleira
+fucinheira = 3 * sapato
+
+print(f"O tamanho do sapato é {sapato} e o tamanho da fucinheira é {fucinheira}.")
