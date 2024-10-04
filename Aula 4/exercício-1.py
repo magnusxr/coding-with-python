7144bfe79dbcecde674b1c657854ab77131efd5e
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-04 19:17:46 -0300

    exercício 1 - elegibilidade

---
+++ Aula 4/exercício-1.py
@@ -0,0 +1,9 @@
+print("Saiba se você pode fazer o curso de Python.")
+
+idade = int(input("Quantos anos você tem? "))
+
+if idade >= 16:
+    print("Você pode fazer o curso!")
+
+else:
+    print("Você não pode fazer o curso.")
