68dbd5d921a4d3aee6ed0476cb15550ceae415c0
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-04 18:10:32 -0300

    dever de casa - bicicleta

---
+++ Aula 3/exercício-4.py
@@ -0,0 +1,9 @@
+aro = int(input("Qual o tamanho do aro que deseja comprar? "))
+
+guidão = aro / 2
+manete = aro * 0.25
+quadro = aro
+
+print(f"O tamanho recomendado para o guidão é de {guidão}.")
+print(f"O tamanho recomendado para o manete é de {manete}.")
+print(f"O tamanho recomendado para o quadro é de {quadro}.")
