a11f2ae1e7d9046b98233c0f9767e9c4dc664349
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-04 19:17:30 -0300

    exemplo 1 - voto obrigatório

---
+++ Aula 4/exemplo-1.py
@@ -0,0 +1,9 @@
+print("Saiba se você tem a obrigação de voto.")
+
+idade = int(input("Qual a sua idade? "))
+
+if 18 <= idade < 70:
+    print("Você é obrigado a votar!")
+
+else:
+    print("Você não é obrigado a votar.")
