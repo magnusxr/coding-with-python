0dd24fda873311f2167c29ce5dd3c5fd80c93283
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-04 23:36:37 -0300

    aula 4 - dever de casa

---
+++ Aula 4/dever.py
@@ -0,0 +1,14 @@
+num = int(input("Digite um número: "))
+
+if num % 3 == 0 and num % 5 == 0:
+    print(f"O número {num} é divisível por 3 e 5!")
+
+else:
+    if num % 3 == 0:
+        print(f"O número {num} é divisível por 3!")
+
+    elif num % 5 == 0:
+        print(f"O número {num} é divisível por 5!")
+    
+    else:
+        print(f"O número {num} não é divisível por 3 ou 5!")
