881d7876d0392b55a29c08ce4827b2d07d0a0dc2
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-07 18:28:36 -0300

    update - código mais otimizado

--- Aula 4/dever.py
+++ Aula 4/dever.py
@@ -12,3 +12,19 @@ else:
     
     else:
         print(f"O número {num} não é divisível por 3 ou 5!")
+
+# mais otimizado
+        
+num = int(input("Digite um número: "))
+
+if num % 3 == 0:
+    print(f"O número {num} é divisível por 3!")
+
+    if num % 5 == 0:
+        print(f"O número {num} é divisível por 5!")
+
+elif num % 5 == 0:
+        print(f"O número {num} é divisível por 5!")
+
+else:
+     print(f"O número {num} não é divisível por 3 ou 5!")
