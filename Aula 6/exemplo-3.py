a14908acc7b96e228193ea7bfeb42ad0bccdeb06
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-09 20:57:08 -0300

    exemplo 3 - multiplicação

---
+++ Aula 6/exemplo-3.py
@@ -0,0 +1,10 @@
+x = 0
+multiplica = 1
+
+while x < 3:
+    numero = float(input("Insira um número: "))
+    x += 1
+
+    multiplica *= numero
+
+    print(f"O resultado da multiplicação é: {multiplica}.")
