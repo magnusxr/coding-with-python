aba84f4f9b3cf2fb5a29eb50e7a69f40927ab89b
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-09 19:55:36 -0300

    exercício 1 - redirecionamento

---
+++ Aula 6/exercício-1.py
@@ -0,0 +1,10 @@
+import time
+
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    time.sleep(1)
+    contador += 1
+
+print("Obrigado por esperar! Estamos redirecionando sua ligação.")
