f028d4c744b9a10c507b0cdbd4480b0ccfbd7e07
Magnus Xavier <magnusxavierr@outlook.com>
2024-10-13 18:51:35 -0300

    Update dever.py

--- Aula 6/dever.py
+++ Aula 6/dever.py
@@ -1,28 +1,42 @@
 # INFORMAR A COR DA LIXEIRA CORRETA
 
-print("Bem-vindo ao programa de reciclagem!")
+def reciclar(material):
+    match material:
+        case "1":
+            print(f"\nLixeira Azul")
 
-material = input("Informe o material que você deseja reciclar:\n1 - Papel\n2 - Plástico\n3 - Vidro\n4 - Metal\n5 - Orgânico\n6 - Resíduos Não-Recicláveis\n> ")
+        case "2":
+            print(f"\nLixeira Vermelha")
 
-match material:
-    case "1":
-        print(f"\nLixeira Azul")
+        case "3":
+            print(f"\nLixeira Verde")
 
-    case "2":
-        print(f"\nLixeira Vermelha")
+        case "4":
+            print(f"\nLixeira Amarela")
 
-    case "3":
-        print(f"\nLixeira Verde")
+        case "5":
+            print(f"\nLixeira Marrom")
 
-    case "4":
-        print(f"\nLixeira Amarela")
+        case "6:":
+            print(f"\nLixeira Cinza")
 
-    case "5":
-        print(f"\nLixeira Marrom")
+        case _:
+            print("\nErro: Comando Inválido. Tente novamente.")
 
-    case "6:":
-        print(f"\nLixeira Cinza")
+while True:
+    print("Bem-vindo ao programa de reciclagem!")
 
-    case _:
+    material = input("Informe o material que você deseja reciclar:\n1 - Papel\n2 - Plástico\n3 - Vidro\n4 - Metal\n5 - Orgânico\n6 - Resíduos Não-Recicláveis\n> ")
+
+    reciclar(material)
+    
+    continuar = input("\nDeseja continuar reciclando? S / N.\n> ").lower
+    
+    if continuar == "s":
+        reciclar(material)
+
+    elif continuar == "n":
+        print("Obrigado por contribuir com a reciclagem!")
+
+    else:
         print("\nErro: Comando Inválido. Tente novamente.")
-
